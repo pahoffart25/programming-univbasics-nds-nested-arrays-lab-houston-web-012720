e514ec1ed50585f820fed6577cd4ceb10e5c4342
Paul Hoffart <pahoffart25@gmail.com>
2019-12-07 00:02:55 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -29,6 +29,11 @@ def array_literal_matrix
   # Using Array literal syntax only, build a nested array that uses the data in
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
   # alphabetically by the first character.
+  combined = [
+    ENNET_HOUSE,
+    ENFIELD_TENNIS_ACADEMY
+  ]
+  combined.sort
 end
 
 def matrix_lookup(matrix, row, column)
